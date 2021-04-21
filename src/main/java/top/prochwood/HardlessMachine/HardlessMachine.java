b1cea9e1cafd689cab40532f79da5b3a306cfe75
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 17:22:34 +0800

    Initial commit

---
+++ src/main/java/top/prochwood/HardlessMachine/HardlessMachine.java
@@ -0,0 +1,16 @@
+package top.prochwood.HardlessMachine;
+
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HardlessMachine extends JavaPlugin implements SlimefunAddon {
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return null;
+    }
+
+    @Override
+    public String getBugTrackerURL() {
+        return null;
+    }
+}
